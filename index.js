f4869253c9cb1150897f1a7f528ceed0fcd2976f
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-12-02 16:21:18 +0100

    slow progress

--- index.js
+++ index.js
@@ -1,3 +1,231 @@
+const movies = [
+
+    {
+
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+
+    Year: "2001",
+
+    imdbID: "tt0120737",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings: The Return of the King",
+
+    Year: "2003",
+
+    imdbID: "tt0167260",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings: The Two Towers",
+
+    Year: "2002",
+
+    imdbID: "tt0167261",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of War",
+
+    Year: "2005",
+
+    imdbID: "tt0399295",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lords of Dogtown",
+
+    Year: "2005",
+
+    imdbID: "tt0355702",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings",
+
+    Year: "1978",
+
+    imdbID: "tt0077869",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of the Flies",
+
+    Year: "1990",
+
+    imdbID: "tt0100054",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lords of Salem",
+
+    Year: "2012",
+
+    imdbID: "tt1731697",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+
+    Year: "1984",
+
+    imdbID: "tt0087365",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of the Flies",
+
+    Year: "1963",
+
+    imdbID: "tt0057261",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Avengers",
+
+    Year: "2012",
+
+    imdbID: "tt0848228",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Infinity War",
+
+    Year: "2018",
+
+    imdbID: "tt4154756",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Age of Ultron",
+
+    Year: "2015",
+
+    imdbID: "tt2395427",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Endgame",
+
+    Year: "2019",
+
+    imdbID: "tt4154796",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+
+    },
+
+    ];
+
 /*
 
 ASSIGNMENT RULES
@@ -356,11 +584,16 @@ let glasses = {
 }
 
 function deleteProp(obj, str) {
-    delete obj[str];
-    return obj;
+    if (typeof obj === "object" && typeof str === "string") {
+        delete obj[str];
+        return obj;
+    } else {
+        return "One or both of the parameters type is wrong, please try again."
+    }
 }
 
 //console.log(deleteProp(glasses, "color"));
+
 
 
 /* EXERCISE 12
@@ -370,7 +603,21 @@ Write a function called oldestMovie which finds the oldest movie in the provided
 */
 printTitle("Exercise 12");
 
+function oldestMovie(movieArr) {
+    if (Array.isArray(movieArr)) {
+        let oldest = movieArr[0];
+        for (let i = 0; i < movieArr.length; i++) {
+            if (movieArr[i].Year > oldest.Year) {
+                oldest = movieArr[i];
+                console.log("so far so good"); 
+            }
+        }
+    } else {
+        return "The parameter needs to be an array. Please try again."
+    }
+}
 
+console.log(`The oldest movie from the list is ${oldestMovie(movies)}`);
 
 /* EXERCISE 13
 
@@ -505,230 +752,3 @@ Commit and push the code to your personal GitHub repository; then post the link 
 
 /* This movies array is used throughout the exercises. You’re not supposed to alter it. */
 
-const movies = [
-
-    {
-
-    Title: "The Lord of the Rings: The Fellowship of the Ring",
-
-    Year: "2001",
-
-    imdbID: "tt0120737",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "The Lord of the Rings: The Return of the King",
-
-    Year: "2003",
-
-    imdbID: "tt0167260",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "The Lord of the Rings: The Two Towers",
-
-    Year: "2002",
-
-    imdbID: "tt0167261",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Lord of War",
-
-    Year: "2005",
-
-    imdbID: "tt0399295",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Lords of Dogtown",
-
-    Year: "2005",
-
-    imdbID: "tt0355702",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "The Lord of the Rings",
-
-    Year: "1978",
-
-    imdbID: "tt0077869",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Lord of the Flies",
-
-    Year: "1990",
-
-    imdbID: "tt0100054",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "The Lords of Salem",
-
-    Year: "2012",
-
-    imdbID: "tt1731697",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
-
-    Year: "1984",
-
-    imdbID: "tt0087365",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Lord of the Flies",
-
-    Year: "1963",
-
-    imdbID: "tt0057261",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "The Avengers",
-
-    Year: "2012",
-
-    imdbID: "tt0848228",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Avengers: Infinity War",
-
-    Year: "2018",
-
-    imdbID: "tt4154756",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Avengers: Age of Ultron",
-
-    Year: "2015",
-
-    imdbID: "tt2395427",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
-
-    },
-
-    {
-
-    Title: "Avengers: Endgame",
-
-    Year: "2019",
-
-    imdbID: "tt4154796",
-
-    Type: "movie",
-
-    Poster:
-
-    "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
-
-    },
-
-    ];
